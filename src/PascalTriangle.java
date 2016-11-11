4f921dc30fb259f7b167e83afb34a0dca96788aa
zcgu <zcgu@users.noreply.github.com>
2016-11-11 00:23:30 -0600

    Update PascalTriangle.java

--- src/PascalTriangle.java
+++ src/PascalTriangle.java
@@ -23,6 +23,9 @@ import java.util.Arrays;
  */
 public class PascalTriangle {
 
+	// The number of rows we want to print.
+	private static final int MAX_LEVEL = 5;
+	
 	/**
 	 * 
 	 * Main class will first initialize and print 'nums' array, 
@@ -32,18 +35,24 @@ public class PascalTriangle {
 	 * @param args : not used.
 	 */
 	public static void main(String[] args) {
-		int[] nums = new int[5];	// Array stores numbers on each level.
+		// Array stores numbers on each level.
+		// Numbers on tree levels are mapping to the smallest array indexes.
+		// For example, for the first three rows, 
+		// nums should be: [1, 0, 0, 0, 0], [1, 1, 0, 0, 0], [1, 2, 1, 0, 0].
+		int[] nums = new int[MAX_LEVEL];	
 		
 		nums[0] = 1;
 		
 		// Get and print the numbers on each level.
-		for (int level = 1; level <= 5; level++) {
+		for (int level = 1; level <= MAX_LEVEL; level++) {
 			if (level > 1) {
 				nums = getNextLevel(nums, level);
 			}
 			
-/*			TODO: Part 1. Use print to help you debug.	*/
-//			System.out.println("Main method, nums: " + Arrays.toString(nums));
+//			TODO: Part 1. Use print to help you debug.
+//			This print statement helps determine whether the problem is in
+//			getNextLevel or printOneLevel.			
+//			System.out.println("Main method, nums: " + Arrays.toString(nums));			
 			
 			printOneLevel(nums, level);
 		}
@@ -58,7 +67,7 @@ public class PascalTriangle {
 	 */
 	private static void printOneLevel(int[] nums, int level) {
 		// Print the spaces at the beginning of each line.
-		for (int i = 0; i < 5 - level; i++) {
+		for (int i = 0; i < MAX_LEVEL - level; i++) {
 			System.out.print(' ');
 		}
 		
@@ -73,25 +82,27 @@ public class PascalTriangle {
 	 * based on current level.
 	 * 
 	 * @param nums : Array stores numbers.
-	 * @param level : The current level.
+	 * @param nextLevel : The number of next level.
 	 */
 	private static int[] getNextLevel(int[] nums, int nextLevel) {
 
-		int[] nextNums = new int[5];  // Array stores numbers on next level.
+		int[] nextNums = new int[MAX_LEVEL];  // Array stores numbers on next level.
 		
 		nextNums[0] = nums[0];
 		
 		// Each number at position i on the next level equals to the sum of
 		// number at position i and i - 1 on the current level,
 		// except the first one and the last one.
-		for (int i = 1; i < nextLevel - 1; i++) {
+		for (int i = 1; i < nextLevel - 2; i++) {
 			nextNums[i] = nums[i - 1] + nums[i];
 		}
 		
 		nextNums[nextLevel - 1] = nums[nextLevel - 2];
 		
-/*		TODO: Part 2. Use print to help you debug.	*/
-//		System.out.println("getNextLevel method, nextNums: " + Arrays.toString(nextNums));
+//		TODO: Part 2. Use print to help you debug.
+//		With the print statement in main we figured out the problem must be in this method.
+//		This print statement is to see if this method is working correctly to this point.
+//		System.out.println("getNextLevel method, nextNums: " + Arrays.toString(nextNums));		
 		
 		return nextNums;
 	}
